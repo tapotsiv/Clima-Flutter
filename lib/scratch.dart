b62ec3464b42e4f7e4a45fc51655be6530a277e7
John Visto <tapotsiv@gmail.com>
2020-05-11 12:18:10 +0800

    added geolocator, ios android location permission

--- lib/scratch.dart
+++ lib/scratch.dart
@@ -11,15 +11,19 @@ void performTasks() {
 }
 
 void task1() {
+  String result = 'Task 1 data';
   print('Task 1 complete');
 }
 
 void task2() {
+  String result = 'Task 2 data';
   Duration threeSeconds = Duration(seconds: 20);
-  Future.delayed(threeSeconds);
-  print('Task 2 complete');
+  Future.delayed(threeSeconds, () {
+    print('Task 2 complete');
+  });
 }
 
 void task3() {
+  String result = 'Task 3 data';
   print('Task 3 complete now');
 }
